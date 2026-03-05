2f6ab69416dc48bad8365615c0b50346d36bcb0a
bayjidalom <bayjidalom1005@gmail.com>
2026-03-05 15:17:59 +0600

    Display dynamic Cards

---
+++ Saved/step-notes.md
@@ -0,0 +1,49 @@
+1. Onclick button -> 
+
+2. loadLessons
+3. displayLessons  
+    -> with 4 steps 👇
+    step-1 get container & empty
+    step-2 get every element by loop (for/forEach)
+    step-3 create element 
+    step-4 append to container
+
+4. loadLevelWord
+6. displayLevelWord
+
+7. 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+### For use dynamically
+
+<div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4">
+            <h2 class="text-2xl font-bold">Eager</h2>
+            <p class="font-semibold ">Meaning /Pronounciation</p>
+
+            <div class="font-bangla text-2xl font-medium">"আগ্রহী / ইগার"</div>
+
+            <div class="flex justify-between items-center">
+
+                <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]">
+                    <i class="fa-solid fa-circle-info"></i>
+                </button>
+
+                <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]">
+                    <i class="fa-solid fa-volume-high"></i>
+                </button>
+
+            </div>
+
+        </div>
--- script/index.js
+++ script/index.js
@@ -13,20 +13,41 @@ const loadLevelWord = (id) => {
     console.log(url)
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data.data))
+        .then(data => displayLevelWord(data.data))
 };
 
 const displayLevelWord = (words) => {
     const wordContainer = document.getElementById("word-container")
     wordContainer.innerHTML = "";
+    // { id: 19, level: 1, word: "Sincere", meaning: "সত্‍ / আন্তরিক", pronunciation: "সিনসিয়ার" }
 
     words.forEach(word => {
+        console.log(word)
+        console.log(word)
         const card = document.createElement('div');
         card.innerHTML = `
-        <p> Cat </p>
+        <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4">
+            <h2 class="text-2xl font-bold">${word.word}</h2>
+            <p class="font-semibold ">Meaning /Pronounciation</p>
+
+            <div class="font-bangla text-2xl font-medium">${word.meaning}/${word.pronunciation}</div>
+
+            <div class="flex justify-between items-center">
+
+                <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]">
+                    <i class="fa-solid fa-circle-info"></i>
+                </button>
+
+                <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]">
+                    <i class="fa-solid fa-volume-high"></i>
+                </button>
+
+            </div>
+
+        </div>
         `;
-        
-        wordContainer.appendChild(card)
+
+        wordContainer.append(card);
 
     });
 
