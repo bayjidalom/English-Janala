e89d3efc6732e2c0401912102dd27608b41d4ff3
bayjidalom <bayjidalom1005@gmail.com>
2026-03-05 13:40:51 +0600

    Load words

--- script/index.js
+++ script/index.js
@@ -2,9 +2,37 @@ const loadLessons = () => {
 
     fetch("https://openapi.programming-hero.com/api/levels/all")   // promise of response
         .then(res => res.json())                // promise of json data
-        .then((json) => displayLessons(json.data))       // cz array in a object named 'data'      // send to line 10
+        .then((json) => displayLessons(json.data))
 
 }
+
+
+const loadLevelWord = (id) => {
+
+    const url = `https://openapi.programming-hero.com/api/level/${id}`
+    console.log(url)
+    fetch(url)
+        .then(res => res.json())
+        .then(data => console.log(data.data))
+};
+
+const displayLevelWord = (words) => {
+    const wordContainer = document.getElementById("word-container")
+    wordContainer.innerHTML = "";
+
+    words.forEach(word => {
+        const card = document.createElement('div');
+        card.innerHTML = `
+        <p> Cat </p>
+        `;
+        
+        wordContainer.appendChild(card)
+
+    });
+
+}
+
+
 
 const displayLessons = (lessons) => {
     // console.log(lessons)
@@ -19,7 +47,7 @@ const displayLessons = (lessons) => {
         const btnDiv = document.createElement('div')
         btnDiv.innerHTML = `
         
-                    <button class="btn btn-outline btn-primary">
+                    <button onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary">
                      <i class="fa-solid fa-book-open"></i> Lesson - ${lesson.level_no}
                     </button>
                         
